d12f285f496c17d5da4b853c890777094ef43bb6
Leandro C. Santana <leandro.c.santana@hotmail.com>
2023-10-14 12:55:36 -0300

    Válidação de usuário na Atualização de tarefa

--- src/main/java/br/com/lcsproject/todolist/task/TaskController.java
+++ src/main/java/br/com/lcsproject/todolist/task/TaskController.java
@@ -40,10 +40,6 @@ public class TaskController {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor que a data de término");
     }
 
-    if(taskModel.getTitle().length() > 50) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O título da tarefa pode ter no máximo 50 caracters");
-    }
-
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(task);
   }
@@ -58,12 +54,22 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
     var task = this.taskRepository.findById(id).orElse(null);
+
+    if(task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+    }
+
+    if(!task.getUserId().equals(userId)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar esse tarefa");
+    }
 
     Utils.copyNonNullProperties(taskModel, task);
 
-    return this.taskRepository.save(task);
+    var taskUpdate = this.taskRepository.save(task); 
+    return ResponseEntity.status(HttpStatus.OK).body(taskUpdate);
   }
 
 }
