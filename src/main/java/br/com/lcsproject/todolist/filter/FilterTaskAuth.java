3c70b8c0f9b898dbc9fea4e2b4a3a3326c54103d
Leandro C. Santana <leandro.c.santana@hotmail.com>
2023-10-13 23:17:32 -0300

    Criando aplicação APIRest de todolist com Java

---
+++ src/main/java/br/com/lcsproject/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,67 @@
+package br.com.lcsproject.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.lcsproject.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    
+    var servlet = request.getServletPath();
+
+    if(servlet.startsWith("/tasks/")) {      
+      //Pegar a autenticação (usuário e senha)
+      var authorization = request.getHeader("Authorization");
+
+      var authEncoded = authorization.substring("Basic".length()).trim();
+
+      byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+      var authString = new String(authDecoded);
+
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+
+      //Válida usuário
+      var user = this.userRepository.findByUsername(username);
+
+      if(user == null) {
+        response.sendError(401);
+      } else {
+        //Válida senha
+        var passwordVerified = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+        if(passwordVerified.verified) {
+          request.setAttribute("userId", user.getId());
+          filterChain.doFilter(request, response);
+        } else {
+        response.sendError(401);
+        }
+      }
+    } else {
+      filterChain.doFilter(request, response);
+    }
+
+  }
+  
+}
---
+++ src/main/java/br/com/lcsproject/todolist/task/TaskController.java
@@ -0,0 +1,69 @@
+package br.com.lcsproject.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lcsproject.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+  
+  @Autowired
+  private ITaskRepository taskRepository;
+
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+    taskModel.setUserId((UUID) userId);
+
+    var currentDate = LocalDateTime.now();
+
+    if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início/término deve ser maior que a data atual");
+    }
+
+    if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor que a data de término");
+    }
+
+    if(taskModel.getTitle().length() > 50) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O título da tarefa pode ter no máximo 50 caracters");
+    }
+
+    var task = this.taskRepository.save(taskModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+
+    var tasks = this.taskRepository.findByUserId((UUID) userId);
+
+    return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
+  }
+
+}
